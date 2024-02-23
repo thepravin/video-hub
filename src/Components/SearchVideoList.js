d73ea8bbc7edf4baf655308b55053c48eaf06d0a
Pravin Nalawade <thepravin28@gmail.com>
2024-02-23 22:47:21 +0530

    search api added

---
+++ src/Components/SearchVideoList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchVideoList = () => {
+  return (
+    <div>SearchVideoList</div>
+  )
+}
+
+export default SearchVideoList
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,9 @@ export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
 
 export const VIDEO_DATA_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key="+YOUTUB_KEY+"&id=";
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+  YOUTUB_KEY +
+  "&id=";
 // "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=[YOUR_API_KEY]";
 
 export const VIDEO_COMMENTS =
@@ -15,3 +17,8 @@ export const VIDEO_COMMENTS =
   YOUTUB_KEY +
   "&textFormat=plainText&part=snippet&maxResults=100&videoId=";
 // "https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyBK4ztdGTTR81CXoOA5NRYDSQudQMb53J4&textFormat=plainText&part=snippet&videoId=upDhKSx7P7E&maxResults=100";
+
+export const SEARCH_VIDEO_LIST =
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" +
+  YOUTUB_KEY +
+  "&q=";
