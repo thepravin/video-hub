2a65a4e09027bf8f2c18159f6d9ef52e16629905
Pravin Nalawade <thepravin28@gmail.com>
2024-02-25 20:13:39 +0530

    favicon added

--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -21,7 +21,7 @@ const Watch = () => {
           <iframe
             width="1000"
             height="500"
-            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")+"?&mute=1&autoplay=1"}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
---
+++ src/SimmerUI/WatchSimmerUI.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WatchSimmerUI = () => {
+  return (
+    <div>WatchSimmerUI</div>
+  )
+}
+
+export default WatchSimmerUI
